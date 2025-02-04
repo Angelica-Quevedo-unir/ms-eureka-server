61d3407c5b2e2ae75b1adec120804f709d5099ac
Angelica Quevedo <angelicadaniela.quevedo190@comunidadunir.net>
2025-02-03 21:54:58 -0500

    :tada: first commit ms-eureka-server

---
+++ src/test/java/es/unir/relatosdepapel/eureka/MsEurekaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package es.unir.relatosdepapel.eureka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsEurekaServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
